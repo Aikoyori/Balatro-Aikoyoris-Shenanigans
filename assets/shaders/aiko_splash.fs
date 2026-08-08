576564e853f37ac1dfcd3bda81212204a4df8da3
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2026-08-09 04:23:02 +0700

    feat: new splash shader

--- assets/shaders/aiko_splash.fs
+++ assets/shaders/aiko_splash.fs
@@ -1,61 +1,110 @@
+#define BLACK 0.6*vec4(79./255.,99./255., 103./255., 1./0.6)
 #if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
-	#define MY_HIGHP_OR_MEDIUMP highp
+	#define PREC highp
 #else
-	#define MY_HIGHP_OR_MEDIUMP mediump
+	#define PREC mediump
 #endif
 
-extern MY_HIGHP_OR_MEDIUMP number time;
-extern MY_HIGHP_OR_MEDIUMP number vort_speed;
-extern MY_HIGHP_OR_MEDIUMP vec4 colour_1;
-extern MY_HIGHP_OR_MEDIUMP vec4 colour_2;
-extern MY_HIGHP_OR_MEDIUMP number mid_flash;
-extern MY_HIGHP_OR_MEDIUMP number vort_offset;
+#define PI 3.14159265358978
 
-#define PIXEL_SIZE_FAC 700.
-#define BLACK 0.6*vec4(79./255.,99./255., 103./255., 1./0.6)
+extern PREC number time;
+extern PREC number vort_speed;
+extern PREC vec4 colour_1;
+extern PREC vec4 colour_2;
+extern PREC number mid_flash;
+extern PREC number vort_offset;
+
+
+#define PIXEL_SIZE_FAC 640.
+#define SPIN_EASE 0.5
+
+
+mat2 rotationMatrix(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat2(c, -s, s, c);
+}
+
+// https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
+// shadertoy fucking broke on me
+
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 
 vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords )
 {
     //Convert to UV coords (0-1) and floor for pixel effect
-    MY_HIGHP_OR_MEDIUMP number pixel_size = length(love_ScreenSize.xy)/PIXEL_SIZE_FAC;
-    MY_HIGHP_OR_MEDIUMP vec2 uv = (floor(screen_coords.xy*(1./pixel_size))*pixel_size - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy);
-    MY_HIGHP_OR_MEDIUMP number uv_len = length(uv);
-
-    //Adding in a center swirl, changes with time
-    
-    MY_HIGHP_OR_MEDIUMP number speed = time*vort_speed  + uv.x * 50.12;
-    //MY_HIGHP_OR_MEDIUMP number new_pixel_angle = atan(uv.y, uv.x) + (2.2 + 0.4*min(6.,speed))*uv_len - 1. -  speed*0.05 - min(6.,speed)*speed*0.02 + vort_offset;
-    
-    MY_HIGHP_OR_MEDIUMP vec2 mid = (love_ScreenSize.xy/length(love_ScreenSize.xy))/2.;
-    MY_HIGHP_OR_MEDIUMP vec2 sv = (mid.xy * 0.1) + uv.xy + vec2(0., sin(speed * 0.6) * 0.006 * pow((1.0 - abs(-uv.x))*2.5,2.0) );
     /*
-    MY_HIGHP_OR_MEDIUMP vec2 sv = vec2((uv_len * cos(new_pixel_angle) + mid.x), (uv_len * sin(new_pixel_angle) + mid.y)) - mid;
+    PREC number pixel_size = length(love_ScreenSize.xy)/1000.0;
+    PREC vec2 uv = (floor(screen_coords.xy*(1./pixel_size))*pixel_size - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy) - vec2(0.12, 0.);
+    PREC number uv_len = length(uv);
     */
-
-	//Now add the smoke effect to the swirled UV
-
-    sv *= 30.;
-    speed = time*(6.)*vort_speed + vort_offset + 1033.;
-	MY_HIGHP_OR_MEDIUMP vec2 uv2 = vec2(sv.x+sv.y);
-
-    for(int i=0; i < 5; i++) {
-		uv2 += sin(max(sv.x, sv.y)) + sv;
-		sv  += 0.5*vec2(cos(5.1123314 + 0.353*uv2.y + speed*0.131121),sin(uv2.x - 0.113*speed));
-		sv  -= 1.0*cos(sv.x + sv.y) - 1.0*sin(sv.x*0.711 - sv.y);
-	}
-
-    //Make the smoke amount range from 0 - 2
-	MY_HIGHP_OR_MEDIUMP number smoke_res =min(2., max(-2., 1.5 + length(sv)*0.12 - 0.17*(min(10.,time*1.2 - 4.))));
-    if (smoke_res < 0.2) {
-        smoke_res = (smoke_res - 0.2)*0.6 + 0.2;
+    //Convert to 0-1.. coordinates but...
+    vec4 tex = vec4(0.,0.,0.,1.);
+    if (mid_flash < 0.0001) {
+        tex.r = tex.r + mid_flash * 0.00001;
     }
+    if (vort_offset < 0.0001) {
+        tex.r = tex.r + vort_offset * 0.00001;
+    }
+    if (vort_speed < 0.0001) {
+        tex.r = tex.r + vort_speed * 0.00001;
+    }
+    if (colour_1.r < 0.0001) {
+        tex.r = tex.r + colour_1.r * 0.00001;
+    }
+    if (colour_2.r < 0.0001) {
+        tex.r = tex.r + colour_2.r * 0.00001;
+    }
+    /*
+    vec2 uv = (screen_coords - uibox_pos) / uibox_size.xy;
+
+    uv.x = uv.x * (love_ScreenSize.x/love_ScreenSize.y);
+    uv.y = uv.y / (20.);
+    */
+    float iTime = time / 15.0;
+    PREC number aspect = love_ScreenSize.x/love_ScreenSize.y ;
+    PREC vec2 uv = screen_coords / vec2(love_ScreenSize.x) ;
     
-    MY_HIGHP_OR_MEDIUMP number c1p = max(0.,1. - 2.*abs(1.-smoke_res));
-    MY_HIGHP_OR_MEDIUMP number c2p = max(0.,1. - 2.*(smoke_res));
-    MY_HIGHP_OR_MEDIUMP number cb = 1. - min(1., c1p + c2p);
+    PREC number pixel_size = length(love_ScreenSize.xy)/PIXEL_SIZE_FAC;
+    uv = (floor(screen_coords.xy*(1./pixel_size))*pixel_size - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy);
+    uv *= 15.1;
+    vec2 uvc = (uv + vec2(0.5,0.5));
+    uvc = uvc * rotationMatrix(-PI * 0.075 * iTime + 0.572);
+    PREC number dist = distance(uvc, vec2(0.5,0.5));
+    
+    uvc = uvc * rotationMatrix(-PI * 0.075 * sin(pow(dist, 1.3)));
+    vec2 color = vec2(step(0.5, fract(uvc.x)),step(0.5, fract(uvc.y)));
+    float luminance = rgb2hsv(tex.rgb).b;
+    number factor = 1.0;
+    if (color.x != color.y){
+        tex.rgb = vec3(1.,0.,1.) * 0.7;
+        //factor = step(0.65,luminance)*0.8+0.4;
+    }
+    number dist_pow = pow((dist * 0.15), 2.4);
+    tex.rgb *= vec3(1.,0.,1.) * (1. - dist_pow);
+    tex.rgb += vec3(1.,0.,1.) * 0.3 * (1. - dist_pow);
 
-    MY_HIGHP_OR_MEDIUMP vec4 ret_col = colour_1*c1p + colour_2*c2p + vec4(cb*BLACK.rgb, cb*colour_1.a);
-    MY_HIGHP_OR_MEDIUMP number mod_flash = max(mid_flash*0.8, max(c1p, c2p)*5. - 4.4) + mid_flash*max(c1p, c2p);
+    //tex.rgb = tex.rgb * (factor + 0.3*((sin(iTime * 2.0 + length(oguv) * 5.0) + 1.0)*0.5));
+    
+    // vec2 uv = (screen_coords - uibox_pos) / uibox_size.xy;
 
-    return ret_col*(1. - mod_flash) + mod_flash*vec4(1., 1., 1., 1.);
+    return tex + vec4(colour.rgb * mid_flash, 1.);
+
 }

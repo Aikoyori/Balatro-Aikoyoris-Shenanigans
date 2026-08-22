2d8777f37171fe3ae1c2bcd578cb6b9a13bc985a
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2026-08-22 20:11:33 +0700

    fix: meta mods only do it

--- meta/main.lua
+++ meta/main.lua
@@ -52,141 +52,143 @@ G.FUNCS.akpop_disable_aikoshen = function(e)
     SMODS.restart_game()
 end
 
-local smods_loc_hook = SMODS.load_mod_localization
-function SMODS.load_mod_localization(paz, modid, depth)
+if not AKPOP.aikoshen_loaded then
+    local smods_loc_hook = SMODS.load_mod_localization
+    function SMODS.load_mod_localization(paz, modid, depth)
 
-    local is_pop_in = false
-    if modid == '_' and (depth or 0) < 1 then
-        for k, v in ipairs(SMODS.mod_list) do
-            if v.id == AKPOP.id then
-                is_pop_in = true
-                break
+        local is_pop_in = false
+        if modid == '_' and (depth or 0) < 1 then
+            for k, v in ipairs(SMODS.mod_list) do
+                if v.id == AKPOP.id then
+                    is_pop_in = true
+                    break
+                end
+            end
+            -- print(is_pop_in and "exist" or "not exist")
+            if not is_pop_in then
+                -- print("added")
+                SMODS.mod_list[#SMODS.mod_list+1] = AKPOP
             end
         end
-        -- print(is_pop_in and "exist" or "not exist")
-        if not is_pop_in then
-            -- print("added")
-            SMODS.mod_list[#SMODS.mod_list+1] = AKPOP
-        end
-    end
 
-    local returnx = {smods_loc_hook(paz, modid, depth)}
+        local returnx = {smods_loc_hook(paz, modid, depth)}
 
-    if modid == AKPOP.id and (depth or 0) < 1 then
-        for k, v in ipairs(SMODS.mod_list) do
-            if v.id == AKPOP.id then
-                -- print("found pop, removing")
-                table.remove(SMODS.mod_list, k)
+        if modid == AKPOP.id and (depth or 0) < 1 then
+            for k, v in ipairs(SMODS.mod_list) do
+                if v.id == AKPOP.id then
+                    -- print("found pop, removing")
+                    table.remove(SMODS.mod_list, k)
+                end
             end
         end
+
+
+        return unpack(returnx)
+        
     end
 
 
-    return unpack(returnx)
-    
-end
-
-
-local gmainmenuhook = Game.main_menu
-function Game:main_menu(chctx)
-    if not AKPOP.aikoshen_loaded then
-        local uinodes = {}
-        local uinodes_text = {}
-        local key = "akpop_warning_stable_outdated_smods"
-        local key_btn = "k_akpop_download_smods"
-        if AKPOP.aikoshen_is_dev_ver then
-            key = "akpop_warning_dev_mod_version"
-            key_btn = "k_akpop_download_aikoshen"
-        end
-        localize{type = "descriptions", key = key, set = "Other", default_col = G.C.WHITE, nodes = uinodes, vars = {}, scale = 2}
-        localize{type = "name", key = key, set = "Other", default_col = G.C.WHITE, nodes = uinodes_text, vars = {}, scale = 2}
-        G.E_MANAGER:add_event(
-            Event{
-                trigger = "after",
-                delay = 0.1,
-                func = function ()
-                    G.FUNCS.overlay_menu{
-                        definition = create_UIBox_generic_options({
-                            contents = {
-                                {
-                                    n = G.UIT.R,
-                                    config = { align = "cr" },
-                                    nodes = {
-                                        UIBox_button({
-                                            label = { "x" },
-                                            minw = 0.5,
-                                            minh = 0.5,
-                                        }),
+    local gmainmenuhook = Game.main_menu
+    function Game:main_menu(chctx)
+        if not AKPOP.aikoshen_loaded then
+            local uinodes = {}
+            local uinodes_text = {}
+            local key = "akpop_warning_stable_outdated_smods"
+            local key_btn = "k_akpop_download_smods"
+            if AKPOP.aikoshen_is_dev_ver then
+                key = "akpop_warning_dev_mod_version"
+                key_btn = "k_akpop_download_aikoshen"
+            end
+            localize{type = "descriptions", key = key, set = "Other", default_col = G.C.WHITE, nodes = uinodes, vars = {}, scale = 2}
+            localize{type = "name", key = key, set = "Other", default_col = G.C.WHITE, nodes = uinodes_text, vars = {}, scale = 2}
+            G.E_MANAGER:add_event(
+                Event{
+                    trigger = "after",
+                    delay = 0.1,
+                    func = function ()
+                        G.FUNCS.overlay_menu{
+                            definition = create_UIBox_generic_options({
+                                contents = {
+                                    {
+                                        n = G.UIT.R,
+                                        config = { align = "cr" },
+                                        nodes = {
+                                            UIBox_button({
+                                                label = { "x" },
+                                                minw = 0.5,
+                                                minh = 0.5,
+                                            }),
+                                        },
                                     },
-                                },
-                                {
-                                    n = G.UIT.R,
-                                    config = { align = "cm" },
-                                    nodes = {transparent_multiline_text(uinodes_text)},
-                                },
-                                {
-                                    n = G.UIT.R,
-                                    config = { align = "cm" },
-                                    nodes = {transparent_multiline_text(uinodes)},
-                                },
-                                {
-                                    n = G.UIT.R,
-                                    config = { align = "cm", padding = 0.1 },
-                                    nodes = {
-                                        {
-                                            n = G.UIT.C,
-                                            config = { align = "cm" },
-                                            nodes = {
-                                                UIBox_button({
-                                                    label = { localize(key_btn) },
-                                                    button = 'akpop_open_link',
-                                                    shadow = 0,
-                                                    minw = 8,
-                                                    colour = G.C.DARK_EDITION,
-                                                    ref_table = {
-                                                        AKPOP.aikoshen_is_dev_ver and
-                                                        "https://github.com/Aikoyori/Balatro-Aikoyoris-Shenanigans/releases/latest" or
-                                                        "https://github.com/Steamodded/smods/releases/latest"
-                                                    }
-                                                }),
+                                    {
+                                        n = G.UIT.R,
+                                        config = { align = "cm" },
+                                        nodes = {transparent_multiline_text(uinodes_text)},
+                                    },
+                                    {
+                                        n = G.UIT.R,
+                                        config = { align = "cm" },
+                                        nodes = {transparent_multiline_text(uinodes)},
+                                    },
+                                    {
+                                        n = G.UIT.R,
+                                        config = { align = "cm", padding = 0.1 },
+                                        nodes = {
+                                            {
+                                                n = G.UIT.C,
+                                                config = { align = "cm" },
+                                                nodes = {
+                                                    UIBox_button({
+                                                        label = { localize(key_btn) },
+                                                        button = 'akpop_open_link',
+                                                        shadow = 0,
+                                                        minw = 8,
+                                                        colour = G.C.DARK_EDITION,
+                                                        ref_table = {
+                                                            AKPOP.aikoshen_is_dev_ver and
+                                                            "https://github.com/Aikoyori/Balatro-Aikoyoris-Shenanigans/releases/latest" or
+                                                            "https://github.com/Steamodded/smods/releases/latest"
+                                                        }
+                                                    }),
+                                                },
                                             },
-                                        },
-                                        {
-                                            n = G.UIT.C,
-                                            config = { align = "cm" },
-                                            nodes = {
-                                                UIBox_button({
-                                                    label = { localize("k_akpop_open_mod_dir") },
-                                                    button = 'akpop_open_mods_folder',
-                                                    func = 'akpop_can_open_mods_folder',
-                                                    minw = 4,
-                                                }),
+                                            {
+                                                n = G.UIT.C,
+                                                config = { align = "cm" },
+                                                nodes = {
+                                                    UIBox_button({
+                                                        label = { localize("k_akpop_open_mod_dir") },
+                                                        button = 'akpop_open_mods_folder',
+                                                        func = 'akpop_can_open_mods_folder',
+                                                        minw = 4,
+                                                    }),
+                                                },
                                             },
-                                        },
-                                        {
-                                            n = G.UIT.C,
-                                            config = { align = "cm" },
-                                            nodes = {
-                                                UIBox_button({
-                                                    label = { localize("k_akpop_disable_aikoshen") },
-                                                    button = 'akpop_disable_aikoshen',
-                                                    minw = 4,
-                                                }),
+                                            {
+                                                n = G.UIT.C,
+                                                config = { align = "cm" },
+                                                nodes = {
+                                                    UIBox_button({
+                                                        label = { localize("k_akpop_disable_aikoshen") },
+                                                        button = 'akpop_disable_aikoshen',
+                                                        minw = 4,
+                                                    }),
+                                                },
                                             },
                                         },
                                     },
                                 },
-                            },
-                            no_back = true,
-                        }),
-                        config = {
-                            no_esc = true,
+                                no_back = true,
+                            }),
+                            config = {
+                                no_esc = true,
+                            }
                         }
-                    }
-                    return true
-                end,
-            }
-        )
+                        return true
+                    end,
+                }
+            )
+        end
+        return gmainmenuhook(self,chctx)
     end
-    return gmainmenuhook(self,chctx)
 end

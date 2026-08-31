cf7ace91d91f127fc76d8ad719f28599521395c9
frostice482 <frostice48260@gmail.com>
2026-09-01 03:30:06 +0700

    improve intervals for word checking and bomb prompt

--- func/bomb_prompts/lib.lua
+++ func/bomb_prompts/lib.lua
@@ -3,7 +3,7 @@ local cu32 = ffi.typeof('const uint32_t*')
 local cvptr = ffi.typeof('const void*')
 local ccptr = ffi.typeof('const char*')
 
-local meta = assert(SMODS.load_file("./func/bomb_prompts/meta.lua"))() --- @type { lendata: any[] }
+local meta = assert(SMODS.load_file("./func/bomb_prompts/meta.lua"))() --- @type AKYRS.bomb_prompt_meta
 local data = SMODS.NFS.newFileData(AKYRS.path..'/func/bomb_prompts/list.bin')
 local dataptr = ccptr(data:getFFIPointer())
 
@@ -11,20 +11,21 @@ local dataptr = ccptr(data:getFFIPointer())
 AKYRS.__data_bomb_prompts = data -- required to prevent gc
 
 for len, lendata in pairs(meta.lendata) do
-	lendata[3] = cu32(cvptr(dataptr + (lendata[1] + len * lendata[2])))
-	if lendata[1] + len * (lendata[2] + 4) > data:getSize() then error("buffer overflow") end
+	local foff = lendata[1] + len * lendata[2]
+	lendata.wordlen = len
+	lendata.offset = lendata[1]
+	lendata.count = lendata[2]
+	lendata.freqs = cu32(cvptr(dataptr + foff))
 end
 
 sendDebugMessage(string.format("Bomb prompt dictionary size (bytes): %d", data:getSize()), "Aikoyori's Shenanigans")
 
---- temporary data entry for each length
---- - [1]: length
---- - [2]: 0-based start index for word
---- - [3]: 0-based end index for word
---- - [4]: weight
---- - [5]: base
-local slots = {}
-for k,v in pairs(meta.lendata) do table.insert(slots, { k, 0, 0, 0 }) end
+--- @type AKYRS.bomb_prompt_meta.state[]
+local states = {}
+for k,v in pairs(meta.lendata) do
+	states[#states+1] = { lendata = v, weight = 0, left = 0, right = 0, baseWeight = 0 }
+end
+table.sort(states, function (a, b) return a.lendata.wordlen < b.lendata.wordlen end)
 
 --- @param index number 0-based
 local function bsearch_freq(index, buf, search)
@@ -33,12 +34,30 @@ local function bsearch_freq(index, buf, search)
 	return (cursum - prevsum) - search
 end
 
+--- @param state AKYRS.bomb_prompt_meta.state
+local function initstate(state, min_freq, max_freq)
+	local freqs = state.lendata.freqs
+	local ri = state.lendata.count-1
+	state.left = AKYRS.binary_search(0, ri, bsearch_freq, 'lL', freqs, min_freq) or 0
+	state.right = AKYRS.binary_search(0, ri, bsearch_freq, 'rL', freqs, max_freq) or ri
+	state.baseWeight = state.left > 0 and AKYRS.endianness.leu32toh(freqs[state.left-1]) or 0
+	state.weight = AKYRS.endianness.leu32toh(freqs[state.right]) - state.baseWeight
+end
+
 --- @param index number 0-based
 local function bsearch_weight(index, buf, search)
 	return AKYRS.endianness.leu32toh(buf[index]) - search
 end
 
-local function sortslot(a, b) return a[1] < b[1] end
+--- @param state AKYRS.bomb_prompt_meta.state
+local function selectstate(state, weight)
+	local ld = state.lendata
+	local freqs = ld.freqs
+	local index = AKYRS.binary_search(state.left, state.right, bsearch_weight, 'lL', freqs, weight + state.baseWeight)
+	local freq = AKYRS.endianness.leu32toh(freqs[index]) - (index > 0 and AKYRS.endianness.leu32toh(freqs[index-1]) or 0)
+	local str = ffi.string(dataptr + ld.offset + index * ld.wordlen, ld.wordlen)
+	return str, freq
+end
 
 function AKYRS.get_bomb_prompt(config)
 	config = config or {}
@@ -48,41 +67,43 @@ function AKYRS.get_bomb_prompt(config)
     local max_length = math.min(config.max_length or 5, 5)
     local min_length = math.max(config.min_length or 2, 2)
 
-	local slotindex = 1
 	local totalweight = 0
-	for len, lendata in pairs(meta.lendata) do
-		local slot = slots[slotindex]
-		slotindex = slotindex + 1
-		slot[4] = 0
+	for i, state in ipairs(states) do
+		state.weight = 0
 
-		if min_length <= len and len <= max_length then
-			local buf = lendata[3]
-			-- lua AKYRS.get_bomb_prompt()
-			local left = AKYRS.binary_search(0, lendata[2]-1, bsearch_freq, 'lL', lendata[3], min_freq)
-			local right = AKYRS.binary_search(0, lendata[2]-1, bsearch_freq, 'rL', lendata[3], max_freq)
-			local base = left > 0 and AKYRS.endianness.leu32toh(buf[left-1]) or 0
-			local weight = AKYRS.endianness.leu32toh(buf[right]) - base
-
-			slot[1], slot[2], slot[3], slot[4], slot[5] = len, left, right, weight, base
-			totalweight = totalweight + weight
+		if min_length <= state.lendata.wordlen and state.lendata.wordlen <= max_length then
+			initstate(state, min_freq, max_freq)
+			totalweight = totalweight + state.weight
 		end
 	end
-	table.sort(slots, sortslot)
 	if totalweight == 0 then return end
 
 	local select = pseudorandom(seed, 1, totalweight)
-	for i, slot in ipairs(slots) do
-		if select > slot[4] then
-			select = select - slot[4]
+	for i, state in ipairs(states) do
+		if select > state.weight then
+			select = select - state.weight
 		else
-			local lendata = meta.lendata[slot[1]]
-			local buf = lendata[3]
-			local index = AKYRS.binary_search(slot[2], slot[3], bsearch_weight, 'lL', lendata[3], select + slot[5])
-			local freq = AKYRS.endianness.leu32toh(buf[index]) - (index > 0 and AKYRS.endianness.leu32toh(buf[index-1]) or 0)
-			local str = ffi.string(dataptr + lendata[1] + index * slot[1], slot[1])
-
-			return str, freq
+			return selectstate(state, select)
 		end
 	end
 	sendWarnMessage("Bomb prompt dictionary not found, weighting failure?", "Aikoyori's Shenanigans")
 end
+
+--- @class AKYRS.bomb_prompt_meta
+--- @field lendata AKYRS.bomb_prompt_meta.lendata[]
+
+--- @class AKYRS.bomb_prompt_meta.lendata
+--- @field wordlen number
+--- byte offset
+--- @field offset number
+--- word count
+--- @field count number
+--- u32 pointer to list of sorted frequency. Length is 4 * count
+--- @field freqs ffi.cdata*
+
+--- @class AKYRS.bomb_prompt_meta.state
+--- @field lendata AKYRS.bomb_prompt_meta.lendata
+--- @field left number
+--- @field right number
+--- @field weight number
+--- @field baseWeight number
--- func/words/lib.lua
+++ func/words/lib.lua
@@ -1,6 +1,6 @@
 local ffi = require("ffi")
 local gcinit = collectgarbage("count")
-local meta = assert(SMODS.load_file("./func/words/meta.lua"))() --- @type { lendata: any[], maxlen: number, charset: string }
+local meta = assert(SMODS.load_file("./func/words/meta.lua"))() --- @type AKYRS.words_meta
 local data = SMODS.NFS.newFileData(AKYRS.path..'/func/words/list.bin')
 local dataptr = ffi.cast("const uint8_t*", data:getFFIPointer())
 local wildchar = string.byte'#'
@@ -21,31 +21,22 @@ for len, lendata in pairs(meta.lendata) do
 			prevbyte = byte
 		end
 	end
-	lendata[3] = nextbuf
-	lendata[4] = charbuf
+	lendata.offset = lendata[1]
+	lendata.count = lendata[2]
+	lendata.wildNextMap = nextbuf
+	lendata.charMap = charbuf
 end
 
 sendDebugMessage(string.format("Word dictionary size (bytes): %d + %d mapping (%d effective) + %dK gc", data:getSize(), mapsize, mapeff, collectgarbage("count") - gcinit), "Aikoyori's Shenanigans")
 
+--- @type AKYRS.words_meta.state
 local state = {
-	-- list of character bytes
 	mem = {},
-	-- string length
 	len = 0,
-	-- related check data
-	-- - [1]: byte offset from dictionary
-	-- - [2]: word length
-	-- - [3]: next wildcard character mapping, byte -> byte, length is 256 * string length. value may be 0 if there's no next wildcard character. first byte in segment always points to first character
-	-- - [4]: valid character mapping, length is 256 * string length
-	lendata = {},
-
-	-- number of bytes to check
+	lendata = {}, --- @type any
 	checklen = 0,
-	-- wild positions, 1-indexed, rightmost to leftmost
 	wildpos = {},
-	-- wild ranges for narrowing binary search, `[start: number, end: number]`, rightmost to leftmost
 	wildrange = {},
-	-- number of wild characters
 	wildcount = 0,
 }
 
@@ -55,7 +46,7 @@ AKYRS.__data_words = data -- required to prevent gc
 --- binary search function
 --- @param index number 0-based
 local function bsearch_entry(index, checklen)
-	local offset = state.lendata[1] + index * state.len
+	local offset = state.lendata.offset + index * state.len
 	for i=1, checklen do
 		local b = state.mem[i]
 		b = dataptr[offset+i-1] - b
@@ -68,7 +59,7 @@ end
 
 --- Search word using binary search, non-wild
 local function bsearch()
-	return AKYRS.binary_search(0, state.lendata[2]-1, bsearch_entry, nil, state.checklen)
+	return AKYRS.binary_search(0, state.lendata.count-1, bsearch_entry, nil, state.checklen)
 end
 
 --- increment next possible characters for wild search
@@ -79,9 +70,9 @@ local function bsearch_nextchar(ix)
 	local prevrange = state.wildrange[ix+1]
 
 	while true do
-		state.mem[pos] = state.lendata[3][(pos-1)*256+state.mem[pos]]
+		state.mem[pos] = state.lendata.wildNextMap[(pos-1)*256+state.mem[pos]]
 		if state.mem[pos] == 0 then
-			state.mem[pos] = state.lendata[3][(pos-1)*256]
+			state.mem[pos] = state.lendata.wildNextMap[(pos-1)*256]
 			if ix == state.wildcount or not bsearch_nextchar(ix+1) then
 				return false
 			end
@@ -116,7 +107,7 @@ local function initstate(word)
 			end
 		else
 			isconswild = false
-			if state.lendata[3][(i-1)*256+c] == 0 then return false end
+			if state.lendata.wildNextMap[(i-1)*256+c] == 0 then return false end
 		end
 		state.mem[i] = c
 	end
@@ -126,7 +117,7 @@ local function initstate(word)
 		if not range then range = {} state.wildrange[state.wildcount+1] = range end
 
 		range[1] = 0
-		range[2] = state.lendata[2]-1
+		range[2] = state.lendata.count-1
 
 		local firstpos = state.wildpos[state.wildcount]-1
 		if firstpos > 0 then
@@ -163,9 +154,46 @@ function AKYRS.check_word(word)
 	end
 	if not i then return { valid = false } end
 
-	local offset = state.lendata[1] + i * len
+	local offset = state.lendata.offset + i * len
 	return {
 		valid = true,
 		word = ffi.string(dataptr+offset, len)
 	}
 end
+
+--- @class AKYRS.words_meta
+--- @field charset string
+--- @field maxlen number
+--- @field lendata AKYRS.words_meta.lendata[]
+
+--- @class AKYRS.words_meta.lendata
+--- byte offset
+--- @field offset number
+--- word count
+--- @field count number
+--- next wildcard character mapping, byte -> byte, length is 256 * string length.
+--- value may be 0 if there's no next wildcard character.
+--- first byte in segment always points to first character
+--- @field wildNextMap ffi.cdata*
+--- valid character mapping, length is 256 * string length.
+--- 1 if character exists in the nth index, 0 if not
+--- @field charMap ffi.cdata*
+
+--- @class AKYRS.words_meta.state
+--- List of character bytes.
+--- Extra values may contain junk
+--- @field mem number[]
+--- String length
+--- @field len number
+---
+--- Related check data
+--- @field lendata AKYRS.words_meta.lendata
+--- Number of bytes to check
+--- @field checklen number
+---
+--- Wild positions, 1-indexed, rghtmost to leftmost
+--- @field wildpos number[]
+--- Wild ranges for narrowing binary search, rightmost to leftmost
+--- @field wildrange [number, number][]
+--- Number of wild characters
+--- @field wildcount number
